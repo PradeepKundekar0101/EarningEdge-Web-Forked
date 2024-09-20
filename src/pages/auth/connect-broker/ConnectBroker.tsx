f432ef83738bf92e34d3fbfbf5ff3f90dab5864b
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2024-09-20 17:14:45 +0530

    Updates in home page

--- src/pages/auth/connect-broker/ConnectBroker.tsx
+++ src/pages/auth/connect-broker/ConnectBroker.tsx
@@ -25,16 +25,23 @@ const ConnectBroker: React.FC = () => {
     mutateAsync: connectBroker,
     isPending,
     error,
-  } = useMutation<ConnectResponse, Error, { clientId: string; accessToken: string }>({
+  } = useMutation<
+    ConnectResponse,
+    Error,
+    { clientId: string; accessToken: string }
+  >({
     mutationKey: ["connectBroker"],
     mutationFn: async ({ clientId, accessToken }) => {
-      const response = await api.post("/broker/connect", { clientId, accessToken });
+      const response = await api.post("/broker/connect", {
+        clientId,
+        accessToken,
+      });
       return response.data;
     },
     onSuccess: (data) => {
       if (data.status === "success") {
         message.success("Connected!");
-        
+
         navigate("/");
       } else {
         message.error("Failed to connect, please try again");
@@ -66,72 +73,88 @@ const ConnectBroker: React.FC = () => {
 
   return (
     <CustomLayout>
-      <div className="flex justify-center items-center min-h-screen bg-darkBg">
-        <div className="bg-darkBg border-[0.4px] border-darkStroke p-8 rounded-lg shadow-md w-full max-w-md">
-          <div className="flex items-center mb-6">
-            <div className="w-10 h-10 bg-green-500 rounded-full flex items-center justify-center text-white text-xl font-bold mr-3">
-              <img
-                src="https://pbs.twimg.com/profile_images/1610246738413248512/0Om-vhfG_400x400.jpg"
-                alt="Broker Logo"
-              />
+      <div className="flex justify-center items-center">
+        {/* <div>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/rAGkne08UXM?si=XFaCz6NqG7dFXuy2"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div> */}
+        <div className="flex justify-center items-center min-h-screen bg-darkBg">
+          <div className="bg-darkBg border-[0.4px] border-darkStroke p-8 rounded-lg shadow-md w-full max-w-md">
+            <div className="flex items-center mb-6">
+              <div className="w-10 h-10 bg-green-500 rounded-full flex items-center justify-center text-white text-xl font-bold mr-3">
+                <img
+                  src="https://pbs.twimg.com/profile_images/1610246738413248512/0Om-vhfG_400x400.jpg"
+                  alt="Broker Logo"
+                />
+              </div>
+              <h2 className="text-2xl font-semibold text-white">
+                Connect to Broker
+              </h2>
             </div>
-            <h2 className="text-2xl font-semibold text-white">
-              Connect to Broker
-            </h2>
+            <form onSubmit={handleConnect}>
+              <div className="mb-4">
+                <label
+                  htmlFor="clientId"
+                  className="block text-sm font-medium text-gray-200 mb-1"
+                >
+                  Client ID
+                </label>
+                <input
+                  type="text"
+                  id="clientId"
+                  className="w-full px-3 py-2 border border-darkStroke rounded-md bg-darkSecondary focus:outline-none focus:ring-2 focus:ring-green-500 text-white"
+                  placeholder="Enter your ClientID"
+                  value={clientId}
+                  onChange={(e) => setClientId(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="accessToken"
+                  className="block text-sm font-medium text-gray-200 mb-1"
+                >
+                  Access Token
+                </label>
+                <input
+                  type="text"
+                  id="accessToken"
+                  className="w-full px-3 py-2 border border-darkStroke text-white rounded-md bg-darkSecondary focus:outline-none focus:ring-2 focus:ring-green-500"
+                  placeholder="Paste your Access token here"
+                  value={accessToken}
+                  onChange={(e) => setAccessToken(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full bg-green-700 mt-2 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+                  disabled={isPending}
+                >
+                  {isPending ? "Connecting..." : "Connect"}
+                </button>
+                <button
+                  onClick={handleHelpClick}
+                  type="button"
+                  className="text-green-400 mt-2 bg-darkSecondary border-darkStroke border-[0.5px] rounded-md w-full py-2 px-4 text-center"
+                >
+                  Need Help?
+                </button>
+              </div>
+            </form>
+            {error && (
+              <p className="mt-4 text-red-500 text-sm">{error.message}</p>
+            )}
           </div>
-          <form onSubmit={handleConnect}>
-            <div className="mb-4">
-              <label
-                htmlFor="clientId"
-                className="block text-sm font-medium text-gray-200 mb-1"
-              >
-                Client ID
-              </label>
-              <input
-                type="text"
-                id="clientId"
-                className="w-full px-3 py-2 border border-darkStroke rounded-md bg-darkSecondary focus:outline-none focus:ring-2 focus:ring-green-500 text-white"
-                placeholder="Enter your ClientID"
-                value={clientId}
-                onChange={(e) => setClientId(e.target.value)}
-                required
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="accessToken"
-                className="block text-sm font-medium text-gray-200 mb-1"
-              >
-                Access Token
-              </label>
-              <input
-                type="text"
-                id="accessToken"
-                className="w-full px-3 py-2 border border-darkStroke text-white rounded-md bg-darkSecondary focus:outline-none focus:ring-2 focus:ring-green-500"
-                placeholder="Paste your Access token here"
-                value={accessToken}
-                onChange={(e) => setAccessToken(e.target.value)}
-                required
-              />
-              <button
-                onClick={handleHelpClick}
-                type="button"
-                className="text-green-400 mt-2"
-              >
-                Need Help?
-              </button>
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-green-700 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-              disabled={isPending}
-            >
-              {isPending ? "Connecting..." : "Connect"}
-            </button>
-          </form>
-          {error && (
-            <p className="mt-4 text-red-500 text-sm">{error.message}</p>
-          )}
         </div>
       </div>
 
@@ -174,18 +197,20 @@ const ConnectBroker: React.FC = () => {
               <h2 className="mb-4 text-lg font-semibold">
                 Existing Account Instructions
               </h2>
-              <p>
-                Please follow the instructions mentioned in the video below:
-              </p>
               <iframe
-                className="w-3/4 my-2"
-                src="https://www.youtube.com/embed/oNB5rzAbAZo?si=Bf5zqvuU64bTHgLE"
+                className="my-2 w-full"
+                // width="560"
+                // height="315"
+                src="https://www.youtube.com/embed/rAGkne08UXM?si=XFaCz6NqG7dFXuy2"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
               ></iframe>
+              <p>
+                Please follow the instructions mentioned in the video below:
+              </p>
               {/* Add video or instructions for existing account holders */}
               <div className="mt-4">
                 <p>1. Log in to your Dhan account</p>
@@ -206,7 +231,16 @@ const ConnectBroker: React.FC = () => {
                 steps:
               </p>
               <div className="mt-4">
-                <p>1. Click here 👉 <a className=" underline font-semibold " target="_blank" href="https://join.dhan.co/?invite=VPNEZ74787">Visit Dhan</a> </p>
+                <p>
+                  1. Click here 👉{" "}
+                  <a
+                    className=" underline font-semibold "
+                    target="_blank"
+                    href="https://join.dhan.co/?invite=VPNEZ74787"
+                  >
+                    Visit Dhan
+                  </a>{" "}
+                </p>
                 <p>2. Click on "Start trading on dhan"</p>
                 <p>3. Create a new account</p>
                 <p>4. Follow the registration process</p>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -50,6 +50,9 @@ const Index: React.FC = () => {
       <AdminAlert />
       <ConnectBroker user={user} />
       <ContactUs firstName={user?.firstName || ""} />
+      <div className="my-4">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/508c10J-ipc?si=HNVSkSca_tWQ38jL" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
       <TopNews />
     </CustomLayout>
   );
